f7563fcce4624ef0a576b876434fe4eac05a26d1
Lorne Zhang <lz150@duke.edu>
2020-03-26 14:17:07 -0700

    added name, email, password, and confirm fields to the User model

--- covid_19_hackathon_app/backend/Users/user.model.js
+++ covid_19_hackathon_app/backend/Users/user.model.js
@@ -3,12 +3,26 @@ const Schema = mongoose.Schema;
 
 // TODO: add more fields to the schema
 const userSchema = new Schema({
-  username: {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
     type: String,
     required: true,
     unique: true,
     trim: true,
-    minlength: 3
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  confirm: {
+    type: String,
+    required: true,
+    minlength: 6
   },
 }, {
   timestamps: true,
