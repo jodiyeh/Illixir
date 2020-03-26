f1b07a54aca8eac5641e5969c2bff542249e5041
Lorne Zhang <lz150@duke.edu>
2020-03-26 14:14:24 -0700

    implemented validation tests using Validator.js. finished user.login.js and user.register.js

--- covid_19_hackathon_app/backend/Users/user.routes.js
+++ covid_19_hackathon_app/backend/Users/user.routes.js
@@ -1,16 +1,47 @@
-const router = require('express').Router();
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+const passport = require("passport");
+
+// Validation
+const validateUserRegister = require("./Validation/user.register");
+const validateUserLogin = require("./Validation/user.login");
+
 let User = require('./user.model');
 
-// TODO: implement create user route
+router.route('/:id').get((req,res) => {
+  User.findById(req.params.id)
+    .then(user => res.json(user))
+    .catch(err => res.status(400).json('Error ' + err));
+});
+
 router.route('/create').post((req,res) => {
+  const username = req.body.username;
+  const newUser = new User({
+    username,
+  });
+  newUser.save()
+    .then(() => res.json('User added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
-// TODO: implement update user route
 router.route('/update/:id').put((req,res) => {
+  User.findById(req.params.id)
+    .then(user => {
+      user.username = req.body.username;
+      user.save()
+        .then(() => res.json('User updated!'))
+        .catch(err => res.status(400).json('Error ' + err));
+    })
+    .catch(err => res.status(400).json('Error ' + err));
 });
 
-// TODO: implement delete user route
 router.route('/delete/:id').delete((req,res) => {
+  User.findByIdAndDelete(req.params.id)
+    .then(() => res.json("User deleted!"))
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 module.exports = router;
--- covid_19_hackathon_app/backend/server.js
+++ covid_19_hackathon_app/backend/server.js
@@ -1,29 +1,40 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require("cors");
+// bodyParser = require("body-parser");
+const passport = require("passport");
 
-const app = express();
+const usersRouter = require('./Users/user.routes');
 const port = process.env.PORT || 5000;
+const app = express();
 
+// Set up middleware: Cors
 app.use('*', cors());
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
 app.use(express.json());
 
+// Config
 require('dotenv').config();
+
+// Set up MongoDB
 const uri = process.env.ATLAS_URI;
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('MongoDB database connection established successfully!'));
+  .catch((reason) => console.log('Unable to connect to the mongodb instance. Error: ', reason));
 
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}
-).catch(function (reason) {
-    console.log('Unable to connect to the mongodb instance. Error: ', reason);
-});
+// Set up routes
+app.use('/api/users', usersRouter);
 
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
-})
-
-const usersRouter = require('./Users/user.routes'); // import exercise route
-app.use('/users', usersRouter);
-
+// Run app
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
