822b01a9408a289e709e91a5e854152a38e67d41
Brian Li <bl195@duke.edu>
2020-04-19 01:10:36 -0400

    pages work

---
+++ covid_19_hackathon_app/src/Components/Home/FAQPage.js
@@ -0,0 +1,18 @@
+import React, {Component } from 'react';
+
+class FAQPage extends Component{
+    render(){
+        return(
+            <div className = "FAQ-page">
+              <div className = "Title-content">
+                <h1 className = "title">#FAQs</h1>
+              </div>
+              <div className = "FAQ-content">
+                
+              </div>
+  
+            </div>
+          
+        )
+    }
+    }
