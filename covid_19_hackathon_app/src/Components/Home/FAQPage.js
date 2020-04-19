6efb94857a417e10d8f8995b01a882222ac8c001
Brian Li <bl195@duke.edu>
2020-04-19 01:51:23 -0400

    added faq

--- covid_19_hackathon_app/src/Components/Home/FAQPage.js
+++ covid_19_hackathon_app/src/Components/Home/FAQPage.js
@@ -13,8 +13,11 @@ class FAQPage extends Component{
                 <h1 className = "FAQ2">How do I use this website after logging in?</h1>
                 <div className = "Answer2">You must first enter you current location's address on the home screen and then you will be given the option to select from a variety of different health-related centers and facilities. After selecting a facility, you will be shown a list of closeby facilities and additional information will be provided upon clicking on a facility's icon.</div>
                 <h1 className = "FAQ3">What kind of information regarding the health facilities are provided?</h1>
-                <div className = "Answer3">This website will provide you with </div>
-                
+                <div className = "Answer3">This website will provide you with a list of facilities sorted in order of relative proximity to your current location. The addresses, coordinates, and proximity of each location will be provided.</div>
+                <h1 className = "FAQ4">What does the Information Page show?</h1>
+                <div className = "Answer4">The Information Page presents a Google Maps view that displays pinpoints of your location as well as various health facilities. You can click on a facility to view additional information.</div>
+                <h1 className = "FAQ5">How can I contact the developers if I have a question or issue?</h1>
+                <div className = "Answer5">In the contacts page, you can submit an email to the developers and you will have your question resolved within 1-2 business days.</div>
               </div>
   
             </div>
