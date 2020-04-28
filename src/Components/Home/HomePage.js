798608f8d62f6a3a0dc28caebe983a83e8f385b5
Lorne Zhang <lz150@duke.edu>
2020-04-27 21:56:30 -0700

    button changes

--- src/Components/Home/HomePage.js
+++ src/Components/Home/HomePage.js
@@ -29,7 +29,7 @@ async function getGeoCode(address) {
     const result = await googleGeoCode(address);
     return result;
   } catch (error) {
-    return "error";
+    return error;
   }
 }
 
@@ -61,7 +61,7 @@ class HomePage extends Component{
     alert("1")
     const geoResponse = getGeoCode(this.state.userStreetAddress + ", " + this.state.userCity + ", " + this.state.userState);
     geoResponse.then((result)=>{
-      alert("2")
+      alert(result)
     const lat = result.json.results[0].geometry.location.lat
     const long = result.json.results[0].geometry.location.lng
     window.location = "/select?state="+this.state.userState+"&city="+this.state.userCity+"&streetAddress="+this.state.userStreetAddress+"&zipcode="+this.state.userZipcode+"&latitude="+lat+"&longitude="+long
