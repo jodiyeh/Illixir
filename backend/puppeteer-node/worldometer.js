d9a3f7d83d05a593ffe16fc058ddb780cad66bd1
Brian Li <bl195@duke.edu>
2020-05-22 01:32:49 -0400

    puppeteer work

--- backend/puppeteer-node/worldometer.js
+++ backend/puppeteer-node/worldometer.js
@@ -6,8 +6,11 @@ puppeteer.launch({
   const page = await browser.newPage();
   await page.goto("https://www.worldometers.info/coronavirus/");
   page.waitForSelector(".maincounter-number").then(async function(){
-    const totalCases = await page.$eval(".maincounter-number span", element => element.innerHTML);
-    console.log(totalCases);
+    // const totalCases = await page.$eval(".maincounter-number span", element => element.innerHTML);
+    const groups = await page.evaluate(() => Array.from(document.getElementsByTagName("maincounter-number"), e => e.textContent));
+    console.log(groups[0]);
+    console.log(groups[1]);
+    console.log(groups[2]);
   })
 }).catch(error => {
   console.log(error);
