5f01c9740d50b47c7e40ecfa64a659ad68f020f4
Lorne Zhang <lz150@duke.edu>
2020-05-22 17:48:55 -0700

    Made clear console log of worldometer data

--- backend/puppeteer-node/worldometer.js
+++ backend/puppeteer-node/worldometer.js
@@ -9,11 +9,17 @@ puppeteer.launch({
     // const totalCases = await page.$eval(".maincounter-number span", element => element.innerHTML);
     //const groups = await page.evaluate(() => Array.from(document.getElementsByTagName(".maincounter-number"), e => e.textContent));
     //const groups = await page.evaluate(() => [...document.querySelector('.maincounter-number').classList]);
+    var mainCounterNumbers = []
     let group = await page.$$('.maincounter-number')
       for (let el of group) {
         let name = await el.$eval(('span'), node => node.innerText.trim());
-        console.log(name)
+        //console.log(name)
+        mainCounterNumbers.push(name);
       }
+    console.log("Coronavirus Data");
+    console.log("Total Cases: " + mainCounterNumbers[0]);
+    console.log("Total Deaths: " + mainCounterNumbers[1]);
+    console.log("Total Recovered: " + mainCounterNumbers[2]);
   })
 }).catch(error => {
   console.log(error);
